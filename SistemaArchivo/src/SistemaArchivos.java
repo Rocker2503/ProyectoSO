d0a739b75bb3bc2626328370673a7c7075f1148b
Juan Ignacio <jcorderuiz@gmail.com>
2017-12-13 03:10:29 -0300

    Listo, estan comentados el read y write, basicamente es un hexadecimal que se transforma a entero y se lee o guarda al disco en esa direccion(no usa eficiencia de memoria en la lectura). Hay un ejemplo de hexadecimal en un print que es el 0x01FE que es la 510 a entero. Para probar con otros hexadecimales se puede usar esta pag: http://www.hexadecimaldictionary.com/hexadecimal FUA

--- SistemaArchivo/src/SistemaArchivos.java
+++ SistemaArchivo/src/SistemaArchivos.java
@@ -5,7 +5,7 @@ import java.util.*;
  *
  * @author Nicolas
  */
-public class SistemaArchivos 
+public class SistemaArchivos
 {
     public static void main(String[] args) 
     {
@@ -84,6 +84,8 @@ public class SistemaArchivos
                     
                     boolean[] ocupados = disco.getOcupados(); 
                     boolean[] arreglo = fat.getOcupados();
+                    
+                    SectorFAT[] bloques = new SectorFAT[512];
                 
                     for (int i = aux; i < division; i++) 
                     {
@@ -106,8 +108,10 @@ public class SistemaArchivos
                         {
                             sfat.setPunteroBloqueFat(i+1);
                         }
+                        bloques[direccionDisco] = sfat;
                     }
                     fat.setOcupados(arreglo);
+                    fat.setBloques(bloques);
                     disco.setOcupados(ocupados);
                     disco.setSectores(sectores);
                     break;
@@ -125,9 +129,78 @@ public class SistemaArchivos
                     break;
                     
                 case 4: //ReadAt
+                    System.out.print("Ingrese una posicion logica offset (por ejemplo 0x01FE): ");
+                    String hex= sc.nextLine();
+                    
+                    //conversión a decimal
+                    int entero = Integer.decode(hex);
+                    System.out.println("entero: " + entero);
+                    
+                    boolean[] ocupadosSF= fat.getOcupados();
+                    int direccionDisco = -1;
+                    
+                    //obtiene la direccion del disco validada
+                    if(fat.getBloques()[entero] != null)
+                    {
+                        direccionDisco = fat.getBloques()[entero].getIndiceDisco();
+                    }
+                    
+                    //si la entrada es menor al largo de los sectoresFAT y efectivamente ese espacio esta ocupado
+                    if(entero < 512 && fat.getBloques()[entero] != null && direccionDisco != -1)
+                    {
+                        //ingresa al disco para leer el archivo
+                        if(disco.getOcupados()[direccionDisco] == true)
+                        {
+                            sectores = disco.getSectores();
+                            for (int i = 0; i < sectores.length; i++) {
+                                if(sectores[i] != null)
+                                {
+                                    Sector s = sectores[i];
+                                    FCB fcb2 = s.getFcb();
+                                    if(fcb2 != null)
+                                    {
+                                        Archivo archi = fcb2.getArchivo();
+                                        System.out.println("");
+                                        System.out.println("Nombre archivo: " + archi.getNombre());
+                                        System.out.println("Descripcion archivo: " + archi.getContenido());
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else if(entero >= 512)
+                        System.out.println("Entrada es mayor al maximo permitido de 512");
+                    else
+                        System.out.println("Espacio no ocupado por un archivo");
                     break;
                     
                 case 5: //WriteAt
+                    System.out.print("Ingrese una posicion logica offset (por ejemplo 0x01FE): ");
+                    hex= sc.nextLine();
+                    
+                    //conversion a decimal
+                    entero = Integer.decode(hex);
+                    System.out.println("entero: " + entero);
+                    
+                    ocupadosSF= fat.getOcupados();
+                    
+                    //obtiene la direccion del disco
+                    direccionDisco = entero;
+                    
+                    //si la entrada es menor al largo de los sectoresFAT y efectivamente ese espacio esta vacio
+                    if(entero < 512 && fat.getBloques()[entero] == null)
+                    {
+                        //ingresa al disco para guardar el archivo en una posicion vacia
+                        if(disco.getOcupados()[direccionDisco] == false)
+                        {
+                            crearArchivo(direccionDisco, sc, fat, mem, disco);
+
+                        }
+                    }
+                    else if(entero >= 512)
+                        System.out.println("Entrada es mayor al maximo permitido de 512");
+                    else
+                        System.out.println("Espacio ya ocupado por un archivo");
                     break;
                     
                 case 6: //PrintFile
@@ -137,13 +210,31 @@ public class SistemaArchivos
                     String res;
                     //muestra el archivo cargado desde memoria
                     res = mostrarContenido(nombre, mem.getSectores(), mem.getTamano(), fat);
+                    if(res != null)
+                        System.out.println("Contenido del archivo: " + res);
                     
                     //si no lo encuentra en memoria lo busca en disco
                     if(res == null)
-                        mostrarContenido(nombre, disco.getSectores(), disco.getTamano(), fat);
+                    {
+                        res = mostrarContenido(nombre, disco.getSectores(), disco.getTamano(), fat);
+                        if(res != null)
+                            System.out.println("Contenido del archivo: " + res);
+                        else
+                            System.out.println("Archivo no encontrado");
+                    }
                     break;
                     
                 case 7: //List
+                    Sector[] sectoresD = disco.getSectores();                    
+                    boolean[] ocupadosD = disco.getOcupados();
+                    
+                    for (int i = 0; i < ocupadosD.length; i++) {
+                        if(ocupadosD[i] == true)
+                        {
+                            System.out.println("Entrada directorio: " + i);
+                            System.out.println("Tamano archivo: " + sectoresD[i].getArchivo().getTamano());
+                        }
+                    }
                     break;
             }
         }
@@ -239,6 +330,98 @@ public class SistemaArchivos
             }
         }
         return null;
+    }
+
+    public static void crearArchivo(int direccionNueva, Scanner sc, FAT fat, Memoria mem, Disco disco) 
+    {
+        System.out.println("");
+        System.out.println("Ingrese nombre del archivo");
+        String nombre = sc.nextLine();
+        while(nombre.length() > 8)
+        {
+            System.out.println("Nombre muy largo, ingrese nombre de nuevo");
+            nombre = sc.nextLine();
+        }
+        System.out.println("Tamaño del archivo");
+        String auxTamano = sc.nextLine();
+        double tamano = Double.parseDouble(auxTamano);
+        System.out.println("Contenido del archivo");
+        String contenido = sc.nextLine();
+
+        //Crear el archivo con los parametros entregados
+        Archivo archivo = new Archivo(tamano, nombre, contenido);
+
+        //Crear un FCB
+        int aux = fatLibre(fat);
+
+        FCB fcb = new FCB(archivo, aux);
+
+
+        Sector sectorFcb = new Sector();
+        sectorFcb.setFcb(fcb);
+
+        Sector[] sectMemoria = mem.getSectores();
+
+        int var = mem.getActual();
+        if(var == 10)
+        {
+            var = 0;
+        }
+        sectMemoria[var] = sectorFcb;
+        mem.setActual(var++);
+
+        int index = discoLibre(disco);
+
+
+        Sector[] sectores = disco.getSectores();
+        sectores[index] = sectorFcb;
+        disco.setSectores(sectores);
+
+        int division = (int)Math.ceil(tamano/512);
+        System.out.println("Bloques de FAT y disco: " + division + "------");
+
+        boolean[] ocupados = disco.getOcupados(); 
+        boolean[] arreglo = fat.getOcupados();
+
+        SectorFAT[] bloques = new SectorFAT[512];
+
+        boolean direccionIngresada = false;
+        int direccionDisco;
+                
+        for (int i = aux; i < division; i++) 
+        {
+            //Se guarda en  la direccion hexadecimal ingresada
+            if(direccionIngresada)
+                direccionDisco = new Random().nextInt(512);
+            else
+            {
+                direccionDisco = direccionNueva;
+                direccionIngresada = true;
+            }
+            while(ocupados[direccionDisco] == true)
+            {
+                direccionDisco = new Random().nextInt(512);
+            }
+            System.out.println("direccion de disco: " + direccionDisco);
+            arreglo[i] = true;
+
+            Sector sectorArchivo = new Sector();
+            sectorArchivo.setArchivo(archivo);
+            sectores[direccionDisco] = sectorArchivo;
+            ocupados[direccionDisco] = true;
+
+
+            SectorFAT sfat = new SectorFAT(direccionDisco);
+            if(i+1 < division)
+            {
+                sfat.setPunteroBloqueFat(i+1);
+            }
+            bloques[direccionDisco] = sfat;
+        }
+        fat.setOcupados(arreglo);
+        fat.setBloques(bloques);
+        disco.setOcupados(ocupados);
+        disco.setSectores(sectores);
     }
 }
 
